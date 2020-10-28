95d13191b7576b8a743c71145fb3c36ab87418a9
Burksson <57063539+Burksson@users.noreply.github.com>
2020-10-28 20:34:09 +0100

    Update random.js

--- random.js
+++ random.js
@@ -1,21 +1,21 @@
 var F = Math.floor(Math.random()*6);
 
 if(F===1) {
-document.getElementById("FW-titel").style.backgroundColor = "springgreen";
+document.getElementById("FW-titel").style.color = "springgreen";
 }
 if(F===2) {
-document.getElementById("FW-titel").style.backgroundColor = "lightblue";
+document.getElementById("FW-titel").style.color = "lightblue";
 }
 if(F===3) {
-document.getElementById("FW-titel").style.backgroundColor = "lightblue";
+document.getElementById("FW-titel").style.color = "lightblue";
 }
 if(F===4) {
-document.getElementById("FW-titel").style.backgroundColor = "lightblue";
+document.getElementById("FW-titel").style.color = "lightblue";
 }
 if(F===5) {
-document.getElementById("FW-titel").style.backgroundColor = "lightblue";
+document.getElementById("FW-titel").style.color = "lightblue";
 }
 if(F<1 || F>5) {
-document.getElementById("FW-titel").style.backgroundColor = "lightblue";
+document.getElementById("FW-titel").style.color = "lightblue";
 }
  
